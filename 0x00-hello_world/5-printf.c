bd9d3cb38aa1bc2c636f2816c0d0af03beecd997
Nonny-50 <dominicoo300@gmail.com>
2022-09-08 09:54:59 -0700

    fifth time

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
 
 /**
  *
- *  * main - Entry point
+ *  * main - Short description, single line
  *
- *   *
+ *   * Description: Longer description of the function)?
  *
- *    * Return: Always 0 (Success)
+ *    * Return: Description of the returned value
  *
  *     */
 
@@ -16,8 +16,8 @@ int main(void)
 
 {
 
-	    printf("with proper grammar, but the outcome is a piece of art,\n");
+		puts("\with proper grammar, but the outcome is a piece of art);
 
-	        return (0);
+			return (0);
 
 }
