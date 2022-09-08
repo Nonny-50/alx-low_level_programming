7d176a12ee1cb43656e8562a4bf6135cf895d3bc
Nonny-50 <dominicoo300@gmail.com>
2022-09-08 08:21:59 -0700

    my puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,23 +4,17 @@
 
 /**
 
- * main - print the string in the put function
+ * main - Entry point
 
  *
 
- * Description: using the main function
-
- * this program prints "Programming is like building a multilingual puzzle
-
- * Return: 0
+ * Return: 0 after printing the function
 
  */
 
 int main(void)
 
-{
-
-    puts(*\""Programming is like building a multilingual puzzle"); 
+{puts"\Programming is like building a multilingual puzzle");
 
     return (0);
 
